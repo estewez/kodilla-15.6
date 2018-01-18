3577415874d614d0d142e891d92c87b9cb670a35
Tomasz <estewez@gmail.com>
2018-01-18 14:00:37 +0100

    Task completed

---
+++ script.js
@@ -0,0 +1,82 @@
+class Stopwatch {
+    constructor(display, timelist) {
+        this.running = false;
+        this.display = display;
+        this.list = timelist;
+        this.reset();
+        this.print(this.times);
+    }
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+        this.print();
+    }
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+    add() {
+        var li = document.createElement('li');
+        li.innerText = this.format(this.times);
+        this.list.appendChild(li);
+    }
+    resetList() {
+        while(this.list.firstChild) {
+            this.list.firstChild.remove();
+        }
+    }
+}
+
+
+
+const stopwatch = new Stopwatch(document.querySelector('.stopwatch'), document.querySelector('.results'));
+
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+var resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', () => stopwatch.reset());
+var addButton = document.getElementById('add');
+addButton.addEventListener('click', () => stopwatch.add());
+var resetListButton = document.getElementById('resetlist');
+resetListButton.addEventListener('click', () => stopwatch.resetList());
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
